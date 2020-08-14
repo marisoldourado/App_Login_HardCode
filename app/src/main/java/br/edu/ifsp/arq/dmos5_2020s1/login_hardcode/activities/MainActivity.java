15e67fad5daaa3f90121558a224a7099f3768a9c
Marisol Dourado <mbernardo@serviceup.com.br>
2020-08-14 13:41:01 -0300

    #added fields in main_activity.xml

---
+++ app/src/main/java/br/edu/ifsp/arq/dmos5_2020s1/login_hardcode/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package br.edu.ifsp.arq.dmos5_2020s1.login_hardcode.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.edu.ifsp.arq.dmos5_2020s1.login_hardcode.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
